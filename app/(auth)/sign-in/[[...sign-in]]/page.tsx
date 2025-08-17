45267bc02f6963def69a5f50b53ee75213e75bce
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-17 23:46:03 +0530

    update

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,17 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "1rem",
+      }}
+    >
+      <SignIn />
+    </div>
+  );
 }
